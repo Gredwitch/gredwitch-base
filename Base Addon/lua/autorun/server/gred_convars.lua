aa614ab747d8cdc6366b0e381690b87e3f5b49ee
Gredwitch <32811753+Gredwitch@users.noreply.github.com>
2018-03-29 17:33:05 +0200

    Moved everything to the base addon folder.

---
+++ Base Addon/lua/autorun/server/gred_convars.lua
@@ -0,0 +1,73 @@
+AddCSLuaFile()
+
+if GetConVar("gred_easyuse") == nil then
+	CreateConVar("gred_easyuse", "1", { FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY } )
+end
+
+if GetConVar("gred_maxforcefield_range") == nil then
+	CreateConVar("gred_maxforcefield_range", "5000", { FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY } )
+end
+
+if GetConVar("gred_fragility") == nil then
+	CreateConVar("gred_fragility", "1", {FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY } )
+end
+
+if GetConVar("gred_shockwave_unfreeze") == nil then
+	CreateConVar("gred_shockwave_unfreeze", "1", { FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY } )
+end
+
+if GetConVar("gred_decals") == nil then
+	CreateClientConVar("gred_decals", "1", true,{ FCVAR_ARCHIVE } )
+end
+
+if GetConVar("gred_sound_shake") == nil then
+	CreateConVar("gred_sound_shake", "1", { FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY } )
+end
+
+if GetConVar("gred_water_impact") == nil then
+	CreateClientConVar("gred_water_impact", "1", true,{ FCVAR_ARCHIVE } )
+end
+
+if GetConVar("gred_he_impact") == nil then
+	CreateConVar("gred_he_impact", "1", { FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY } )
+end
+
+if GetConVar("gred_tracers") == nil then
+	CreateConVar("gred_tracers", "5", { FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY } )
+end
+
+if GetConVar("gred_oldrockets") == nil then
+	CreateConVar("gred_oldrockets", "0", { FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY } )
+end
+
+if GetConVar("gred_jets_speed") == nil then
+	CreateConVar("gred_jets_speed", "1", { FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY } )
+end
+
+if GetConVar("gred_fire_effect") == nil then
+	CreateClientConVar("gred_fire_effect", "1", true,{ FCVAR_ARCHIVE } )
+end
+
+if GetConVar("gred_multiple_fire_effect") == nil then
+	CreateClientConVar("gred_multiple_fire_effects", "1", true,{ FCVAR_ARCHIVE } )
+end
+
+if GetConVar("gred_rocket_explode_waterimpact") == nil then
+	CreateClientConVar("gred_rocket_explode_waterimpact", "1", true,{ FCVAR_ARCHIVE } )
+end
+
+if GetConVar("gred_bomb_explode_waterimpact") == nil then
+	CreateClientConVar("gred_bomb_explode_waterimpact", "1", true,{ FCVAR_ARCHIVE } )
+end
+
+if GetConVar("gred_healthslider") == nil then
+	CreateConVar("gred_healthslider", "100", { FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY } )
+end
+
+if GetConVar("gred_enablehealth") == nil then
+	CreateConVar("gred_enablehealth", "1", { FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY } )
+end
+
+if GetConVar("gred_enableenginehealth") == nil then
+	CreateConVar("gred_enableenginehealth", "1", { FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_NOTIFY } )
+end
---
+++ Base Addon/lua/entities/wac_pod_gunner/shared.lua
@@ -0,0 +1,44 @@
+ENT.Base = "wac_pod_base"
+ENT.Type = "anim"
+ENT.PrintName = ""
+ENT.Author = wac.author
+ENT.Category = wac.aircraft.spawnCategory
+ENT.Contact = ""
+ENT.Purpose = ""
+ENT.Instructions = ""
+ENT.Spawnable = false
+ENT.AdminSpawnable = false
+ENT.Name = "Red Gunner Pod"
+ENT.Ammo = 750
+ENT.FireRate = 730
+ENT.Spray = 0.3
+ENT.FireOffset = Vector(60, 0, 0)
+ENT.TkAmmo = 1
+ENT.TracerColor = "Red"
+ENT.BulletType = "wac_base_12mm"
+ENT.Sounds = {
+	shoot1p = "WAC/cannon/viper_cannon_1p.wav",
+	shoot3p = "WAC/cannon/viper_cannon_3p.wav",
+	spin = "WAC/cannon/viper_cannon_rotate.wav"
+}
+
+function ENT:SetupDataTables()
+	self:base("wac_pod_base").SetupDataTables(self)
+	self:NetworkVar("Float", 2, "SpinSpeed");
+	self:NetworkVar( "String", 0, "TracerColor" );
+	self:NetworkVar( "String", 0, "BulletType" );
+	self:NetworkVar( "Int", 0, "TkAmmo" );
+end
+
+
+
+function ENT:drawCrosshair()
+	surface.SetDrawColor(255,255,255,150)
+	local center = {x=ScrW()/2, y=ScrH()/2}
+	surface.DrawLine(center.x+10, center.y, center.x+30, center.y)
+	surface.DrawLine(center.x-30, center.y, center.x-10, center.y)
+	surface.DrawLine(center.x, center.y+10, center.x, center.y+30)
+	surface.DrawLine(center.x, center.y-30, center.x, center.y-10)
+	surface.DrawOutlinedRect(center.x-10, center.y-10, 20, 20)
+	surface.DrawOutlinedRect(center.x-11, center.y-11, 22, 22)
+end
